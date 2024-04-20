6541641e9604fb1d112262b328a48d081b5b26a4
HoangKhang <khanggplc@gmail.com>
2024-04-20 23:28:44 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Anh cảm ơn ạ😊";
+  document.getElementById("link").innerHTML = "👉Bấm vào đây đi nè👈"; 
+  gif.src =
+    "https://media1.giphy.com/media/iCVzZwwE6QNAV2tEE0/giphy.gif";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  question.innerHTML = "Cho bấm lại😢"
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+  document.getElementById("link").innerHTML = "";
+});
